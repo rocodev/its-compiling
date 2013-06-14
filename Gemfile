45582d35b36e77a6374d291c9e68c986a09728a9
xdite <xuite.joke@gmail.com>
2013-06-14 19:53:06 +0800

    theme

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+
+group :development do
+  gem 'compass', '~> 0.12.2'
+end
---
+++ config.rb
@@ -0,0 +1,14 @@
+# configuration for Compass
+project_type = :stand_alone
+
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
+
+sass_dir = "sass"
+images_dir = "images"
+fonts_dir = "fonts"
+
+line_comments = false
+output_style = :compressed
